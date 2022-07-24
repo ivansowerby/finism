aa2379190490096d5cf8815dde7f1d9cc01f804e
Ivan <ivanlsowerby@gmail.com>
2022-07-24 16:08:42 +0100

    Delete

--- backend/src/cryptism/fng.py
+++
@@ -1,8 +0,0 @@
-NAME = 'name'
-
-VALUE = 'value'
-CLASSIFICATION = 'value_classification'
-TIMESTAMP = 'timestamp'
-TIME_UNTIL_UPDATE = 'time_until_update'
-
-ERROR = 'error'
--- backend/src/stockism/history.py
+++
@@ -1,6 +0,0 @@
-OPEN = 'Open'
-HIGH = 'High'
-LOW = 'Low'
-CLOSE = 'Close'
-ADJACENT_CLOSE = 'Adj Close'
-VOLUME = 'Volume'
